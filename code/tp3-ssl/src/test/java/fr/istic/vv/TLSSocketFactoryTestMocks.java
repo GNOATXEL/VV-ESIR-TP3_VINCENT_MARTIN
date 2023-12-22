628ddbd0c4d9e3f1133ae2832ae23da4beae80a1
Gnaxel ASMR <66784969+GNOATXEL@users.noreply.github.com>
2023-12-22 18:47:51 +0100

    Update TLSSocketFactoryTestMocks.java
    code ajouté

--- code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
+++ code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
@@ -2,14 +2,41 @@ package fr.istic.vv;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class TLSSocketFactoryTestMocks {
+  /**
+   * Test quand tout est nul (cas limite)
+   */
+  @Test
+  public void preparedSocket_NullProtocols()  {
+    SSLSocket mock = mock(SSLSocket.class);
 
+    when(mock.getSupportedProtocols()).thenReturn(null);
+    when(mock.getEnabledProtocols()).thenReturn(null);
+
+    TLSSocketFactory f = new TLSSocketFactory();
+    f.prepareSocket(mock);
+
+    // On check que setEnabledProtocols n'est pas appelée
+    verify(mock, never()).setEnabledProtocols(any(String[].class));
+  }
+
+  /**
+   * Test quand ça n'est pas nul
+   */
+  @Test
+  public void preparedSocket_NotNullProtocols()  {
+    SSLSocket mock = mock(SSLSocket.class);
+
+    when(mock.getSupportedProtocols()).thenReturn(new String[]{"SSLv2Hello", "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"});
+    when(mock.getEnabledProtocols()).thenReturn(new String[]{"SSLv3", "TLSv1"});
+
+    TLSSocketFactory f = new TLSSocketFactory();
+    f.prepareSocket(mock);
+
+    // Check que c'est appelé cette fois
+    verify(mock).setEnabledProtocols(any(String[].class));
+  }
 
 }
